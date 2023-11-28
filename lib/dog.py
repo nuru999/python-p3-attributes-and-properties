b0a2ab70a3b2eafae224221c7b4e232d61d561dd
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-28 15:33:17 +0300

    Attempted

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+     def __init__(self, name = None, breed = None):
+        self._name = name
+        self._breed = breed
+        if name is not None:
+            self.set_name(name)
+        if breed is not None:
+            self.set_breed(breed)
+     def set_name(self, name):
+        if type(name) == str and 0 < len(name) < 25:
+            self._name = name  
+        else:
+            print("Name must be string between 1 and 25 characters.")
+     def get_name(self):
+        return self._name
+     name = property(get_name, set_name)
+
+     def set_breed(self, breed):
+         if breed in APPROVED_BREEDS:
+            self._breed = breed
+         else:
+            print("Breed must be in list of approved breeds.")
+
+     def get_breed(self):
+        return self._breed
+     breed = property(get_breed, set_breed)
